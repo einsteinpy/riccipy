f537557c6f6795b75f77106ca43567cc70a64e1e
cjayross <calvinjayross@gmail.com>
2019-07-05 15:14:57 -0500

    reformatted

--- src/riccipy/numerical.py
+++ src/riccipy/numerical.py
@@ -1,18 +1,22 @@
-import numpy as np
-
-from types import FunctionType
 from functools import partial
 from itertools import starmap
+from types import FunctionType
+
+import numpy as np
 from sympy import lambdify
 from sympy.tensor.tensor import TensExpr
+
 from .tensor import expand_tensor
+
 
 class NumericalArray(object):
     def __init__(self, args, expr, idxs=None, **kwargs):
         self._vars = args
         self._objstr = str(expr)
-        self._array = expand_tensor(expr, idxs=idxs) if isinstance(expr, TensExpr) else expr
-        self._modules = kwargs.pop('modules', None)
+        self._array = (
+            expand_tensor(expr, idxs=idxs) if isinstance(expr, TensExpr) else expr
+        )
+        self._modules = kwargs.pop("modules", None)
         self._lambda = lambdify(self._vars, self._array, modules=self._modules)
         self._generator = self._build_generator()
 
@@ -33,13 +37,14 @@ class NumericalArray(object):
     def __getattr__(self, attr):
         if hasattr(self._array, attr):
             return getattr(self._array, attr)
-        raise AttributeError('%s has no attribute: %s', self.__class__.__name__, attr)
+        raise AttributeError("%s has no attribute: %s", self.__class__.__name__, attr)
 
     def __repr__(self):
-        return 'NumericalArray(%s)' % self._objstr
+        return "NumericalArray(%s)" % self._objstr
 
     def __str__(self):
         return self.__repr__()
+
 
 def lambdify_tensor(args, expr, idxs=None, **kwargs):
     return NumericalArray(args, expr, idxs, **kwargs)
