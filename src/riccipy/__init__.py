6d6dda183927297972aae73083886c92e422f27b
cjayross <calvinjayross@gmail.com>
2019-07-07 16:27:58 -0500

    added load_metric to __init__.py

--- src/riccipy/__init__.py
+++ src/riccipy/__init__.py
@@ -7,5 +7,5 @@ Python library for Tensor Manipulation in General Relativity
 """
 
 __version__ = "0.1.dev0"
-from .metric import Metric, SpacetimeMetric
+from .metric import Metric, SpacetimeMetric, load_metric
 from .tensor import Index, Tensor, expand_tensor, indices
