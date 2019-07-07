5948c4d2087602a308bf81870886ca296804d14b
cjayross <calvinjayross@gmail.com>
2019-07-07 17:06:44 -0500

    changed order of coordinates in einstein_maxwell

--- src/riccipy/metrics/einstein_maxwell_1.py
+++ src/riccipy/metrics/einstein_maxwell_1.py
@@ -1,12 +1,12 @@
 # Static cylindrically symmetric Einstein-Maxwell field (i) angular magnetic field (caused by an axial current)
 from sympy import cosh, diag, log, symbols
 
-coords = symbols("t phi rho z", real=True)
+coords = symbols("t rho z phi", real=True)
 variables = symbols("a b m", constant=True)
 functions = ()
-t, ph, rh, z = coords
+t, rh, z, ph = coords
 a, b, m = variables
 expr1 = cosh(log(a * rh ** m)) ** 2
 expr2 = rh ** (2 * m ** 2) * b ** 2 * expr1
-metric = diag(-expr2, rh ** 2 * b ** 2 * expr1, expr2, 1 / (b ** 2 * expr1))
+metric = diag(-expr2, expr2, 1 / (b ** 2 * expr1), rh ** 2 * b ** 2 * expr1)
 del expr1, expr2, t, ph, rh, z, a, b, m
