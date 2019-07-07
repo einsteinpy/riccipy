fb6b2dcd6a7a550492ddc805d87cbcd21068a83a
cjayross <calvinjayross@gmail.com>
2019-07-07 00:27:03 -0500

    added the remaining metrics

--- src/riccipy/metrics/robertson_walker_1.py
+++ src/riccipy/metrics/robertson_walker_1.py
@@ -1,5 +1,4 @@
 # Closed Friedman-Robertson-Walker metric. Perfect fluid
-<<<<<<< HEAD
 # Robertson, Astrophys. J., v82, p284, (1935)
 # Robertson, Astrophys. J., v83, p137, (1936)
 # Stephani (10.9) p118
@@ -13,12 +12,4 @@ a = functions
 metric = diag(
     -1, a(t) ** 2, a(t) ** 2 * sin(r) ** 2, a(t) ** 2 * sin(r) ** 2 * sin(th) ** 2
 )
-=======
-_coords = symbols('t r theta phi', real=True)
-_vars = ()
-_funs = symbols('a', cls=Function)
-t, r, th, ph = _coords
-a = _funs
-_metric = diag(-1, a(t)**2, a(t)**2*sin(r)**2, a(t)**2*sin(r)**2*sin(th)**2)
->>>>>>> added first metrics
 del t, r, th, ph, a
--- src/riccipy/metrics/robertson_walker_2.py
+++ src/riccipy/metrics/robertson_walker_2.py
@@ -1,5 +1,4 @@
 # Spatially flat Friedman-Robertson-Walker metric. Perfect fluid
-<<<<<<< HEAD
 # Robertson, Astrophys. J., v82, p284, (1935)
 # Robertson, Astrophys. J., v83, p137, (1936)
 # Stephani (10.9) p118
@@ -11,12 +10,4 @@ functions = symbols("a", cls=Function)
 t, r, th, ph = coords
 a = functions
 metric = diag(-1, a(t) ** 2, a(t) ** 2 * r ** 2, a(t) ** 2 * r ** 2 * sin(th) ** 2)
-=======
-_coords = symbols('t r theta phi', real=True)
-_vars = ()
-_funs = symbols('a', cls=Function)
-t, r, th, ph = _coords
-a = _funs
-_metric = diag(-1, a(t)**2, a(t)**2*r**2, a(t)**2*r**2*sin(th)**2)
->>>>>>> added first metrics
 del t, r, th, ph, a
