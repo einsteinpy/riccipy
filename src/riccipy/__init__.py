5c2833cc4302cd968d37f29040973c25743b1423
cjayross <calvinjayross@gmail.com>
2019-07-07 17:29:51 -0500

    added lambdify_tensor to __init__.py

--- src/riccipy/__init__.py
+++ src/riccipy/__init__.py
@@ -8,4 +8,5 @@ Python library for Tensor Manipulation in General Relativity
 
 __version__ = "0.1.dev0"
 from .metric import Metric, SpacetimeMetric
+from .numerical import lambdify_tensor
 from .tensor import Index, Tensor, expand_tensor, indices
