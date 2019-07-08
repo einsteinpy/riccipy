8ec584aae31ce27d0c66330d45a36a8a93492553
cjayross <calvinjayross@gmail.com>
2019-07-07 23:15:17 -0500

    added some documentation

--- src/riccipy/numerical.py
+++ src/riccipy/numerical.py
@@ -10,10 +10,35 @@ from .tensor import expand_tensor
 
 
 class NumericalArray(object):
-    def __init__(self, args, expr, idxs=None, **kwargs):
+    """
+    Class for representing an array of symbolic expressions as lambdified functions.
+
+    Calling instances of this class will result in the evaluation of a lambda function
+    that returns a numerically valued array representing the results of the individual
+    component expressions.
+
+    Furthermore, instances may also be used to access specific components in a manner
+    identical to accessing the components of an numpy array.
+
+    """
+
+    def __init__(self, args, array, **kwargs):
+        """
+        Create a new NumericalArray.
+
+        Parameters
+        ----------
+        args : (list, tuple)
+            Iterable of ~sympy.Symbol objects that specifcy the order of the arguments
+            for the lambda functions that will be generated.
+        array : (list, tuple, ~sympy.Matrix, ~sympy.Array)
+            The array of symbolic expressions to be lambdified.
+
+        """
+        array = Array(array)
         self._vars = args
-        self._objstr = str(expr)
-        self._array = expr
+        self._objstr = str(array)
+        self._array = array
         self._modules = kwargs.pop("modules", None)
         self._lambda = lambdify(self._vars, self._array, modules=self._modules)
         self._generator = self._build_generator()
@@ -45,8 +70,44 @@ class NumericalArray(object):
 
 
 def lambdify_tensor(args, expr, idxs=None, **kwargs):
+    """
+    Generate a numerical array representation for the result of a tensor expression.
+
+    When the tensor expression passed results in an instance of ~sympy.Array, a
+    NumericalArray object is returned.
+    A NumericalArray can be used to access lambda functions for either the array
+    as a whole or for individual components. See the example below.
+
+    Parameters
+    ----------
+    args : (list, tuple)
+        Iterable of ~sympy.Symbol objects that specifcy the order of the arguments
+        for the lambda functions that will be generated.
+    expr : (~sympy.tensor.tensor.TensExpr, ~sympy.Expr)
+        The tensor expression to be lambdified. If this is an ordinary expression
+        this function defaults to ~sympy.lambdify.
+    idxs : (list, tuple)
+        Iterable of Index objects to specify the indices for the result of ``expr``.
+
+    Examples
+    --------
+    >>> t, r, th, ph = symbols('t r theta phi', real=True)
+    >>> schw = diag(1-1/r, -1/(1-1/r), -r**2, -r**2*sin(th)**2)
+    >>> g = Metric('g', (t, r, th, ph), schw)
+    >>> narr = lambdify_tensor((r, th), g(-mu,-nu))
+    >>> narr(2, 0)
+    array([[ 0.5,  0. ,  0. ,  0. ],
+            [ 0. , -2. ,  0. ,  0. ],
+            [ 0. ,  0. , -4. ,  0. ],
+            [ 0. ,  0. ,  0. , -0. ]])
+    >>> narr[0,0]
+    <function _lambdifygenerated(r, theta)>
+    >>> narr[0,0](2, 0)
+    0.5
+
+    """
     if isinstance(expr, TensExpr):
-        expr = expand_tensor(expr)
+        expr = expand_tensor(expr, idxs)
     if isinstance(expr, Array):
-        return NumericalArray(args, expr, idxs, **kwargs)
+        return NumericalArray(args, expr, **kwargs)
     return lambdify(args, expr, **kwargs)
