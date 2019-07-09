66f1a979170e867378d467966b39308157c82c46
cjayross <calvinjayross@gmail.com>
2019-07-08 19:53:50 -0500

    fixed typo in static_spherical_1

--- src/riccipy/metrics/static_spherical_1.py
+++ src/riccipy/metrics/static_spherical_1.py
@@ -6,7 +6,7 @@ from sympy import Function, diag, exp, sin, symbols
 
 coords = symbols("t r theta phi", real=True)
 variables = ()
-functions = symbolic("alpha beta", cls=Function)
+functions = symbols("alpha beta", cls=Function)
 t, r, th, ph = coords
 al, be = functions
 metric = diag(-exp(2 * al(r)), exp(2 * be(r)), r ** 2, r ** 2 * sin(th) ** 2)
